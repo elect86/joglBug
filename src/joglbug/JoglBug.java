660413ed5b149dd080e37fe3b5d5c011457c81c0
Giuseppe Barbieri <elect86@gmail.com>
2016-01-12 10:57:59 +0100

    Rock&Roll

---
+++ src/joglbug/JoglBug.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joglbug;
+
+import com.jogamp.nativewindow.util.Dimension;
+import com.jogamp.newt.Display;
+import com.jogamp.newt.NewtFactory;
+import com.jogamp.newt.Screen;
+import com.jogamp.newt.opengl.GLWindow;
+import static com.jogamp.opengl.GL2ES2.GL_FRAGMENT_SHADER;
+import static com.jogamp.opengl.GL2ES2.GL_VERTEX_SHADER;
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.util.Animator;
+import com.jogamp.opengl.util.glsl.ShaderCode;
+import com.jogamp.opengl.util.glsl.ShaderProgram;
+
+public class JoglBug implements GLEventListener {
+
+    private static int screenIdx = 0;
+    private static Dimension windowSize = new Dimension(1024, 768);
+    private static boolean undecorated = false;
+    private static boolean alwaysOnTop = false;
+    private static boolean fullscreen = false;
+    private static boolean mouseVisible = true;
+    private static boolean mouseConfined = false;
+    public static GLWindow glWindow;
+    public static Animator animator;
+
+    public static void main(String[] args) {
+
+        Display display = NewtFactory.createDisplay(null);
+        Screen screen = NewtFactory.createScreen(display, screenIdx);
+        GLProfile glProfile = GLProfile.get(GLProfile.GL4);
+        GLCapabilities glCapabilities = new GLCapabilities(glProfile);
+        glWindow = GLWindow.create(screen, glCapabilities);
+
+        glWindow.setSize(windowSize.getWidth(), windowSize.getHeight());
+        glWindow.setPosition(50, 50);
+        glWindow.setUndecorated(undecorated);
+        glWindow.setAlwaysOnTop(alwaysOnTop);
+        glWindow.setFullscreen(fullscreen);
+        glWindow.setPointerVisible(mouseVisible);
+        glWindow.confinePointer(mouseConfined);
+        glWindow.setVisible(true);
+
+        JoglBug joglBug = new JoglBug();
+        glWindow.addGLEventListener(joglBug);
+
+        animator = new Animator(glWindow);
+        animator.start();
+    }
+
+    private int program;
+    private final String SHADERS_ROOT = "/shaders";
+
+    public JoglBug() {
+
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        System.out.println("init");
+
+        GL4 gl4 = drawable.getGL().getGL4();
+
+        initProgram(gl4);
+
+        gl4.glEnable(GL4.GL_DEPTH_TEST);
+    }
+
+    private void initProgram(GL4 gl4) {
+
+        ShaderCode vertShader = ShaderCode.create(gl4, GL_VERTEX_SHADER, this.getClass(),
+                SHADERS_ROOT, null, "draw-image-space-rendering", "vert", null, true);
+        ShaderCode fragShader = ShaderCode.create(gl4, GL_FRAGMENT_SHADER, this.getClass(),
+                SHADERS_ROOT, null, "draw-image-space-rendering", "frag", null, true);
+
+        ShaderProgram shaderProgram = new ShaderProgram();
+        shaderProgram.add(vertShader);
+        shaderProgram.add(fragShader);
+
+        shaderProgram.init(gl4);
+
+        program = shaderProgram.program();
+
+        shaderProgram.link(gl4, System.out);
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+        System.out.println("dispose");
+
+        GL4 gl4 = drawable.getGL().getGL4();
+
+        gl4.glDeleteProgram(program);
+
+        System.exit(0);
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+
+        GL4 gl4 = drawable.getGL().getGL4();
+
+        gl4.glClearColor(0f, .33f, 0.66f, 1f);
+        gl4.glClearDepthf(1f);
+        gl4.glClear(GL4.GL_COLOR_BUFFER_BIT | GL4.GL_DEPTH_BUFFER_BIT);
+
+        gl4.glUseProgram(program);
+
+        gl4.glUseProgram(0);
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL4 gl4 = drawable.getGL().getGL4();
+        gl4.glViewport(x, y, width, height);
+    }
+}
